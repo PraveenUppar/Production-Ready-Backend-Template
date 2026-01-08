33f3c4b141c565101d3900563703fd45f954dcaf
Praveen Uppar <praveenuppar718@gmail.com>
2026-01-08 21:20:01 +0530

    rate limitng

--- src/app.ts
+++ src/app.ts
@@ -4,19 +4,33 @@ import helmet from 'helmet';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
+import rateLimit from 'express-rate-limit';
 import logger from './utils/logger.js';
 import globalErrorHandler from './middlewares/error.middleware.js';
 import AppError from './utils/AppError.js';
 import userRoute from './routes/user.route.js';
 import todoRoute from './routes/todo.route.js';
+import { prisma } from './libs/prisma.js';
+import redis from './redis/redis.js';
 
 dotenv.config();
 
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 min
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true,
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: 'Too many requests from this IP, please try again after 15 minutes',
+});
+
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(limiter);
 
 const morganFormat = ':method :url :status :response-time ms';
 
@@ -36,6 +50,27 @@ app.use(
     },
   }),
 );
+
+app.get('/health', async (req: Request, res: Response) => {
+  try {
+    // Check DB connection
+    await prisma.$queryRaw`SELECT 1`;
+    // Check Redis connection
+    await redis.ping();
+
+    res.status(200).json({
+      status: 'active',
+      uptime: process.uptime(),
+      db: 'connected',
+      redis: 'connected',
+    });
+  } catch (error) {
+    res.status(503).json({
+      status: 'Inactive',
+      error: 'Service unavailable',
+    });
+  }
+});
 
 app.use('/api/v1/auth', userRoute);
 app.use('/api/v1', todoRoute);
