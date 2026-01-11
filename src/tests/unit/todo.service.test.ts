61c24ea94af2d955f5309cf44e0f212c5a437b75
Praveen Uppar <praveenuppar718@gmail.com>
2026-01-11 12:10:31 +0530

    unit-test: getTodoService

--- src/tests/unit/todo.service.test.ts
+++ src/tests/unit/todo.service.test.ts
@@ -1,7 +1,7 @@
 jest.mock('../../libs/prisma');
 jest.mock('../../redis/redis');
 
-import { createTodoService } from '../../services/todo.service';
+import { createTodoService, getTodoService } from '../../services/todo.service';
 
 import { prisma } from '../../libs/prisma';
 import redis from '../../redis/redis';
@@ -12,7 +12,7 @@ describe('Todo Service - Unit Tests', () => {
   });
 
   describe('createTodoService', () => {
-    it('creates todo and clears cache', async () => {
+    it('Creates todo and clears cache', async () => {
       const mockTodo = {
         id: '1',
         title: 'Learn Jest',
@@ -37,6 +37,37 @@ describe('Todo Service - Unit Tests', () => {
       });
 
       expect(result).toEqual(mockTodo);
+    });
+  });
+
+  describe('getTodoService', () => {
+    it('Returns cached data when cache exists', async () => {
+      const cachedResult = {
+        data: [{ id: '1', title: 'Cached Todo' }],
+        meta: { totalItems: 1 },
+      };
+      (redis.get as jest.Mock).mockResolvedValue(cachedResult);
+
+      const result = await getTodoService(1, 5);
+
+      expect(redis.get).toHaveBeenCalled();
+      expect(prisma.$transaction).not.toHaveBeenCalled();
+      expect(result).toEqual(cachedResult);
+    });
+
+    it('Fetch from DB and sets cache when cache is empty', async () => {
+      const todos = [{ id: '1', title: 'DB Todo' }];
+      const total = 1;
+
+      (redis.get as jest.Mock).mockResolvedValue(null);
+      (prisma.$transaction as jest.Mock).mockResolvedValue([todos, total]);
+
+      const result = await getTodoService(1, 5);
+
+      expect(prisma.$transaction).toHaveBeenCalled();
+      expect(redis.set).toHaveBeenCalled();
+      // expect(result.data).toEqual(todos);
+      // expect(result.meta.totalItems).toBe(1);
     });
   });
 });
